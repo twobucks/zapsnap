622f3c010e0b1ec9a72a5b9c6785374f5ebc396d
Hrvoje Šimić <shime@twobucks.co>
2016-01-05 20:49:03 +0100

    start seeding stuff

--- client/index.js
+++ client/index.js
@@ -1,1 +1,18 @@
-alert('hi there')
+var Webtorrent = require('webtorrent')
+var client = new Webtorrent()
+
+var img = document.getElementById("img")
+client.seed(new Buffer(img.src), {name: "image"}, function(torrent){
+  console.log('seeding ' + torrent.infoHash)
+
+  client.add(torrent.infoHash, function(torrent){
+    console.log('downloading ' + torrent.infoHash)
+
+    torrent.files.forEach(function (file) {
+      file.getBuffer(function (er, buf) {
+        var img2 = document.getElementById('img2')
+        img2.src = buf.toString()
+      })
+    })
+  })
+})
--- server/index.js
+++ server/index.js
@@ -1,13 +1,64 @@
 var express = require('express')
 var exphbs  = require('express-handlebars');
 var path = require('path')
+var multiparty = require('multiparty')
+var fs = require('fs')
+var util = require('util')
+var uuid = require('uuid')
+var db = require('monk')('localhost/seedshot')
+var bodyParser = require('body-parser')
 
 var app = express()
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: "Inbox Pie",
   })
+})
+
+app.get('/images/:uuid', function(req, res){
+  var images = db.get('images')
+  console.log(req.params.uuid)
+  images.findOne({ uuid: req.params.uuid }).on('success', function (doc) {
+    res.render("index", {
+      base64: doc.base64,
+      infoHash: doc.infoHash
+    })
+  });
+})
+
+app.post('/images/:uuid', function(req, res){
+  console.log(req.body)
+  var hash = req.body.infoHash
+  var images = db.get('images')
+  images.update({
+    uuid: req.params.uuid
+  }, {
+    uuid: req.params.uuid,
+    infoHash: hash
+  }, function(){
+    res.json({
+      infoHash: hash
+    })
+  })
+})
+
+app.post('/', function(req, res){
+  var form = new multiparty.Form();
+  var images = db.get('images')
+  var id = uuid.v1()
+  form.parse(req, function(err, fields, files) {
+    images.insert({
+      uuid: id,
+      base64: fs.readFileSync(files["file"][0]["path"]).toString('base64')
+    }, function(err, doc){
+      if (err) throw err;
+    })
+    res.json({
+      message: "ok",
+      uuid: id
+    })
+  });
 })
 
 app.set('views', path.join(__dirname, 'views'))
