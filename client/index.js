325123c09bbc9440c9bdd13c224344d66d7dd93f
Hrvoje Šimić <shime@twobucks.co>
2016-01-06 17:27:50 +0100

    remove listify

--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 var Webtorrent = require('webtorrent')
 var client = new Webtorrent()
 var request = require('request')
-var listify = require('listify')
 
 var img = document.getElementById("img")
 var seededImg = document.getElementById('img2')
