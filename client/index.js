a9cc2b76077d433df45b6d564421871affc7cd77
Hrvoje Šimić <shime@twobucks.co>
2016-01-07 15:41:19 +0100

    start showing number of peers

--- client/index.js
+++ client/index.js
@@ -2,11 +2,29 @@ var Webtorrent = require('webtorrent')
 var client = new Webtorrent()
 var request = require('request')
 
+
 var img = document.getElementById("img")
 var seededImg = document.getElementById('img2')
 
+var status = document.querySelector('.status')
+function updateSpeed (torrent) {
+  status.innerHTML =
+    '<b>Peers:</b> ' + torrent.swarm.wires.length
+}
+
 if (seededImg){
   client.add(seededImg.src, function(torrent){
+    torrent.swarm.on('download', function(){
+      updateSpeed(torrent)
+    })
+    torrent.swarm.on('upload', function(){
+      updateSpeed(torrent)
+    })
+    setInterval(function(){
+      updateSpeed(torrent)
+    }, 5000)
+    updateSpeed(torrent)
+
     console.log('downloading ' + torrent.infoHash)
 
     torrent.files.forEach(function (file) {
@@ -18,6 +36,17 @@ if (seededImg){
 } else {
   client.seed(new Buffer(img.src), {name: "image"}, function(torrent){
     console.log('seeding ' + torrent.infoHash)
+    torrent.swarm.on('download', function(){
+      updateSpeed(torrent)
+    })
+    torrent.swarm.on('upload', function(){
+      updateSpeed(torrent)
+    })
+    setInterval(function(){
+      updateSpeed(torrent)
+    }, 5000)
+    updateSpeed(torrent)
+
     request.post(window.location.href, {
       body: {
         infoHash: torrent.magnetURI
